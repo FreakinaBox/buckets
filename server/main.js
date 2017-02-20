e61140c2972b2f6963ef844d5054657ed71970cf
Stephen <Stephen@oregonburls.com>
2017-02-20 14:31:28 -0800

    disabled https and using port from env

--- server/main.js
+++ server/main.js
@@ -1,9 +1,13 @@
 "use strict";
 
 const fs = require('fs');
+const http = require('http');
 const http2 = require('spdy');
 const app = require('./components/app');
 const Socket = require('./components/Socket');
+
+const port = process.env.PORT || 5000;
+
 
 //initialize Socket.IO
 
@@ -11,11 +15,12 @@ const Socket = require('./components/Socket');
 require('express-livereload')(app, {watchDir: 'client'});
 
 //start in httpd mode
-let options = {
-	key: fs.readFileSync('localhost.key'),
-	cert: fs.readFileSync('localhost.cert')
-};
-const server = http2.createServer(options, app).listen(443);
+// let options = {
+// 	key: fs.readFileSync('localhost.key'),
+// 	cert: fs.readFileSync('localhost.cert')
+// };
+// const server = http2.createServer(options, app).listen(443);
+const server = http.createServer(app).listen(port);
 const socket = new Socket(server);
 
 // server.listen(80);
